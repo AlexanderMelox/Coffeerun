ba48ca60163a2ebac4a362c85fad430b25761af0
Alexander Melo <alexandermelox@gmail.com>
2018-03-16 12:56:14 -0400

    added deliver order method to truck

--- scripts/truck.js
+++ scripts/truck.js
@@ -15,6 +15,12 @@
     this.db.add(order.emailAddress, order);
   };
 
+  // Deliver order
+  Truck.prototype.deliverOrder = function(customerId) {
+    console.log('Delivering order for ' + customerId);
+    this.db.remove(customerId);
+  }
+
   App.Truck = Truck;
   window.App = App;
 
