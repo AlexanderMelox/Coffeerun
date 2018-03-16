9b76f001c671cc55d2fb5dfa7df27ed24311d17e
Alexander Melo <alexandermelox@gmail.com>
2018-03-16 12:46:12 -0400

    added the truck module

---
+++ scripts/truck.js
@@ -0,0 +1,14 @@
+(function(window) {
+  'use strict';
+
+  const App = window.App || {};
+
+  // Truck constructor
+  function Truck() {
+
+  }
+
+  App.Truck = Truck;
+  window.App = App;
+  
+})(window);
